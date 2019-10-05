0b17a7971f17f32ae2c71d935fa8e2e088a46075
hanchao.li <hanchao.li@borngroup.com>
2019-10-05 15:40:51 -0400

    IL-005: add success return code

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -14,6 +14,7 @@ router.post('/register', async (ctx) => {
         nickname: v.get('body.nickname')
     };
     const r = await User.create(user);
+    throw new global.errs.Success();
 });
 
 module.exports = router;
