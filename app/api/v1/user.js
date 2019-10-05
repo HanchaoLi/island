278d60831778bbda0c137ff64c41897b535f0722
hanchao.li <hanchao.li@borngroup.com>
2019-10-05 15:27:36 -0400

    IL-004: hash the passowrd

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const Router = require('koa-router');
 const {RegisterValidator} = require('../../validators/validator');
 const {User} = require('../../modules/user');
@@ -7,9 +8,13 @@ const router = new Router({
 
 router.post('/register', async (ctx) => {
     const v = await new RegisterValidator().validate(ctx);
+
+    // 10 is cost of generating a salt
+    const salt = bcrypt.genSaltSync(10);
+    const psw = bcrypt.hashSync(v.get('body.password2'), salt);
     const user = {
         email: v.get('body.email'),
-        password: v.get('body.password2'),
+        password: psw,
         nickname: v.get('body.nickname')
     };
     const r = await User.create(user);
