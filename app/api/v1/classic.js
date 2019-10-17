7d32588074f7607803dbcaf1b14143c71205bc86
hanchao.li <hanchao.li@borngroup.com>
2019-10-16 22:19:50 -0400

    IL-008 add test API to get latest publications

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -2,14 +2,24 @@ const Router = require('koa-router');
 const router = new Router({
     prefix: '/v1/classic'
 });
+const {
+    Flow
+} = require('../../modules/flow');
 
 const {
     PositiveIntegerValidator
 } = require('../../validators/validator');
-const {Auth} = require('../../../middlewares/auth');
+const {
+    Auth
+} = require('../../../middlewares/auth');
 
 router.get('/latest', new Auth().m, async (ctx, next) => {
-    ctx.body = ctx.auth.uid;
+    const flow = await Flow.findOne({
+        order:[
+            ['index', 'DESC']
+        ]
+    });
+    ctx.body = flow;
 });
 
 module.exports = router;
