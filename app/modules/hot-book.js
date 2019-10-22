89afe68765484dc6fd54d1b58d74aa8fb6b83824
HanchaoLi <seraphme1234@gmail.com>
2019-10-22 18:08:26 -0400

    part of book API implementation

--- app/modules/hot-book.js
+++ app/modules/hot-book.js
@@ -5,8 +5,26 @@ const {
     Sequelize,
     Model
 } = require('sequelize');
+const {Favor} = require('./favor');
 
 class HotBook extends Model {
+    static async getAll() {
+        const books = await HotBook.findAll({
+            order:[
+                'index'
+            ]
+        });
+        const ids = [];
+        books.forEach((book) => {
+            ids.push(book.id);
+        });
+        Favor.findAll({
+            where: {
+                [Op.in]: ids
+            }
+        });
+        
+    }
 
 }
 
