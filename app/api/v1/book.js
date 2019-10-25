3294ce870e38532c338ecb2106cc5d63452a31f1
HanchaoLi <seraphme1234@gmail.com>
2019-10-25 13:52:58 -0400

    IL-012 add get book detail API

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,15 +1,29 @@
 const Router = require('koa-router');
 const router = new Router({
-    prefix: 'v1/book'
+    prefix: '/v1/book'
 });
 
-const {HotBook} = require('../../modules/hot-book');
+const {
+    PositiveIntegerValidator
+} = require('../../validators/validator');
+const {
+    HotBook
+} = require('../../modules/hot-book');
+const {
+    Book
+} = require('../../modules/book');
 
 router.get('/hot_list', async (ctx, next) => {
     const books = await HotBook.getAll();
     ctx.body = {
         books: books
     };
+});
+
+router.get('/:id/detail', async (ctx, next) => {
+    const v = await new PositiveIntegerValidator().validate(ctx);
+    const book = await new Book(v.get('path.id'));
+    ctx.body = await book.detail();
 });
 
 module.exports = router;
