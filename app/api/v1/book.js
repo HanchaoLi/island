2cdba315a6a850ff13c5e72526bc1a1557fd97cf
HanchaoLi <seraphme1234@gmail.com>
2019-10-28 20:12:35 -0400

    IL-014: add get favor book count and get favor book status API

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -13,6 +13,12 @@ const {
 const {
     Book
 } = require('../../modules/book');
+const {
+    Favor
+} = require('../../modules/favor');
+const {
+    Auth
+} = require('../../../middlewares/auth');
 
 router.get('/hot_list', async (ctx, next) => {
     const books = await HotBook.getAll();
@@ -31,6 +37,23 @@ router.get('/search', async (ctx, next) => {
     const v = await new SearchValidator().validate(ctx);
     const result = await Book.searchFromYuShu(v.get('query.q'), v.get('query.start'), v.get('query.count'));
     ctx.body = result;
+});
+
+router.get('/favor/count', new Auth().m, async ctx => {
+    const count = await Book.getMyFavorBookCount(ctx.auth.uid);
+    ctx.body = {
+        count
+    };
+});
+
+router.get('/:book_id/count', new Auth().m, async ctx => {
+    const v = await new PositiveIntegerValidator().validate(ctx, {
+        id: 'book_id'
+    });
+    const favor = await Favor.getBookFavor(ctx.auth.uid, v.get('path.book_id'));
+    ctx.body = {
+        favor
+    };
 });
 
 module.exports = router;
--- app/modules/book.js
+++ app/modules/book.js
@@ -9,6 +9,8 @@ const {
     Op
 } = require('sequelize');
 
+const {Favor} = require('./favor');
+
 class Book extends Model {
     constructor(id) {
         super();
@@ -19,6 +21,17 @@ class Book extends Model {
         const detail = await axios.get(url);
         return detail.data;
     }
+
+    static async getMyFavorBookCount(uid) {
+        const count = await Favor.count({
+            where: {
+                type: 400,
+                uid
+            }
+        });
+        return count;
+    }
+
     static async searchFromYuShu(q, start, count, summary=1) {
         const url = util.format(global.config.yushu.keywordUrl, encodeURI(q), count, start, summary);
         const result = await axios.get(url);
